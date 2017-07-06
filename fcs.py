ed392abad2fedd5417931d8a03afaf837946c8a3
JD Russo <jrusso@udel.edu>
2017-07-06 14:21:17 -0400

    Refactored code to track individual lipid intensities

--- fcs.py
+++ fcs.py
@@ -26,7 +26,10 @@ k = w_z/w_xy
 epsilon = .5
 
 # Step size when iterating through frames
-STEP = 20
+STEP = 25
+
+# Percentage of tagged particles
+SAMPLING_RATIO = .37
 
 def main():
     print("Running")
@@ -41,17 +44,35 @@ def main():
     #   at the center of the phosphate group.
     #   Error from this would be on the order of the bond lengths, so roughly
     #   1.5 angstrom.
-    phosphorous_atoms = [a.index for a in t.topology.atoms if a.element.symbol == 'P']
-    t.restrict_atoms(phosphorous_atoms)
+    print("Starting with %d atoms" % t.topology.n_atoms)
 
-    # This will be a list of detections per timestep
-    detections = []
+    phosphorous_atoms = [a.index for a in t.topology.atoms if a.element.symbol == 'P']
+    print(phosphorous_atoms)
+    t.atom_slice(phosphorous_atoms, inplace=True)
+
+    print("Reduced to %d phosphorous atoms" % t.topology.n_atoms)
+
+    # Reduce to the sampling ratio
+    num_sampled = int(t.topology.n_atoms * SAMPLING_RATIO)
+
+    # Randomly select the sampled atoms
+    sampled = np.random.choice([a.index for a in t.topology.atoms], num_sampled)
+    # sampled = [a.index for a in t.topology.atoms if a.index % (num_sampled) == 0]
+    t.atom_slice(sampled, inplace=True)
+
+    print("Reduced to %d \"tagged\" phosphorous atoms" % t.topology.n_atoms)
+
+    # This will be a list of lists of detections for each residue per timestep
+    #   Use a list comprehension here instead of [[]]*<some number> to deep copy the
+    #   empty list.
+    detections = [ [] for x in t.topology.residues ]
 
     # Iterate through each timestep (frame, in Gromacs terms)
     for frame_index in range(0,len(t), STEP):
-        print("Processing frame %d out of %d" % (frame_index/STEP, len(t)/STEP))
+        print("Processing frame %d out of %d" % (frame_index/STEP, len(t)/STEP), end="\r")
 
-        detected = 0
+        # Initialize this to -1, since it increments at the beginning of the residue loop
+        idx = -1
 
         # Iterate through each residue
         #   TODO: May want to track each residue independently. This can be
@@ -59,17 +80,20 @@ def main():
         #   and appending each residue to its own list within detections.
         for residue in t.topology.residues:
 
+            # Increment this here, so that it'll increment even if we skip the residue
+            idx += 1
+
             # Do analysis if atom is in detection volume
             if not check_in_detection_volume(t, frame_index, residue):
+                print("Not in detection volume, skipping.")
                 continue
 
-            detected += generate_detection(t, frame_index, residue)
-            print("\r%d detections on frame %d\r" % (detected, frame_index), end="")
+            detected = generate_detection(t, frame_index, residue)
+            detections[idx].append(detected)
 
-        detections += [detected]
         print("")
 
-    np.savetxt("dataout.dat", detections)
+    # np.savetxt("dataout.dat", detections)
 
     return detections
 
@@ -111,6 +135,6 @@ def generate_detection(t, frame_index, residue):
 
 if __name__ == "__main__":
     detections = main()
-    plt.plot(detections)
+    for data in detections:
+        plt.plot(np.arange(0,len(data), 1), data)
     plt.show()
-    print(detections)
